89417a9bc30e7162f480b54d5da644b04105a6ae
Wufasa <mxw373@case.edu>
2018-11-12 23:39:39 -0800

    Added attack and reset button placeholders

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,22 +12,22 @@ var pokemon = {
     charmander:{
         hp: 20,
         attack: 30,
-        counter: 20,
+        defense: 20,
     },
     bulbasaur:{
         hp: 110,
         attack: 30,
-        counter: 20,
+        defense: 20,
     },
     squirtle:{
         hp: 220,
         attack: 30,
-        counter: 20,
+        defense: 20,
     },
     pikachu:{
         hp:420,
         attack:430,
-        counter:420,
+        defense:420,
     }
 }
 var player = [];
@@ -41,22 +41,47 @@ function set(){
     
 }
     
-    function battle(pick){ 
+    function setPlayer(pick){ 
         $("#text-header").text("Choose your enemy");
         $(".view").css("background-image", "url(assets/images/pokemon-gym-background-9.png)");
 //        $(pick).hide();
-        $("#player").append(pick)
+        $("#player").html(pick)
+        $("#p-hp").text("HP: " + player.hp);
+        $("#p-attack").text("Attack: " + player.attack);
+        $("#p-defense").text("Defense: " + player.defense);
 
+    }
+    
+    function setEnemy(pick){
+        $("#text-header").text("Battle!");
+        $("#enemy").html(pick);
+        $(".blank").empty(); //not emptying charmander
+        $("#e-hp").append("HP: " + enemy.hp);
+        $("#e-attack").append("Attack: " + enemy.attack);
+        $("#e-defense").append("Defense: " + enemy.defense);
     }
 
     //select player and enemy
 $(".pokemon").on("click",function(){
     if (player.length < 1){
         player = pokemon[$(this).attr("id")]; //store player attr
-        battle(this); //set battle
+        setPlayer(this); //set battle
+    }
+    else if(enemy.length < 1){//enemy has been chosen
+        enemy = pokemon[$(this).attr("id")];
+        setEnemy(this);//store enemy values
+        //display enemy, display attack button
+        
+        
     }
     else{
-        console.log("hi");
+        //Do nothing;
     }
     })
+    
+    
+    //attack button code
+    //when a kill is scored, redisplay pokemon
+    //win condition
+    //reset button
 })
